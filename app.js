027f7d9ba2f31e007394d08134e5ba13e0a702b2
llleeeaaannn <piercefrank10@gmail.com>
2023-12-07 00:48:28 +0000

    Updated temperature of model and changed prompt to only return value.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import express from 'express';
 import generateTextCalories from './controllers/text.js';
 
-generateTextCalories('100 grams of cooked steak');
+generateTextCalories('Plate of steak with roast potatoes.');
--- controllers/text.js
+++ controllers/text.js
@@ -1,14 +1,16 @@
 import openai from '../config/openai.js';
 
+
 const generateTextCalories = async (food) => {
 
   const caloriesCompletion = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     max_tokens: 20,
+    temperature: 0,
     messages: [
       {
         role: 'user',
-        content: `Calculate how many calories are in ${food}. Just give the number, nothing else."`
+        content: `Calculate how many calories are in ${food}. Just give the number, nothing else. Do not include the word calories, simply provide the integer."`
       }
     ],
   })
