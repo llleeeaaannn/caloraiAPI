43143b3fec063e6130bddeb7910c1e5cb8f28898
llleeeaaannn <piercefrank10@gmail.com>
2023-12-11 23:42:04 +0000

    Built out text route logic

--- routes/text.js
+++ routes/text.js
@@ -1,1 +1,13 @@
+import express from 'express';
 import generateTextCalories from '../controllers/generateTextCalories.js';
+
+const router = express.config();
+
+const getCalories = async function (req, res, next) {
+  try {
+    generateTextCalories();
+  } catch (error) {
+    console.log(error.message);
+    next(error)
+  }
+}
