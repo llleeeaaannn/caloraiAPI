81622e8203adfdbc4aa50ff673ba622219527ee4
llleeeaaannn <piercefrank10@gmail.com>
2023-12-11 23:31:11 +0000

    Defined app and imported cors

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+import cors from 'cors';
 import express from 'express';
 import generateTextCalories from './controllers/text.js';
+
+// Define express app and use cors & json
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 generateTextCalories('Plate of steak with roast potatoes.');
