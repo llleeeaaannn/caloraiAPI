ec8a364fa50c4cd169847101489aaf67b7de17a9
llleeeaaannn <piercefrank10@gmail.com>
2023-12-11 23:32:57 +0000

    Added error handling in app.js

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Error handling middleware
+const errorHandling = (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+}
+
 generateTextCalories('Plate of steak with roast potatoes.');
